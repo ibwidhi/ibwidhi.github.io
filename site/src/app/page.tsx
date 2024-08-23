059e5a93289ac5cfb90cfc1c0b365a57e9da4aa3
Widhi Adnyana <tudewidhi@gmail.com>
2024-08-23 12:06:28 +0800

    initial commit for site

---
+++ site/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <div className="font-roboto">Hello World</div>
+    </main>
+  );
+}
