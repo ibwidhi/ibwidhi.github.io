6cc0de891a9aba02ddf0e3ab7fc92bdd8920e9e9
Widhi Adnyana <tudewidhi@gmail.com>
2024-09-05 10:45:33 +0800

    chore: Hello Github

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="h-screen flex items-center justify-center">
       <div className="flex flex-col items-center justify-center">
-        <div className="font-roboto">Hello World</div>
+        <div className="font-roboto">Hello Github</div>
         <div>
           <Image src={`./github.png`} alt="github" width="64" height="64" />
         </div>
