994a598ffeceb90481ab341b2ad53df5a77337e6
Widhi Adnyana <tudewidhi@gmail.com>
2024-09-05 09:52:11 +0800

    feat: test add image

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="h-screen flex items-center justify-center">
-      <div className="font-roboto">Hello World</div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="font-roboto">Hello World</div>
+        <div>
+          <Image src={`/github.png`} alt="github" width="64" height="64" />
+        </div>
+      </div>
     </main>
   );
 }
